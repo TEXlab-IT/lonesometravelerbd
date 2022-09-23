ca450162dc5a6ec1acf2c98b15c68f57c6e821c1
Kamrul Hasan <kamrul.ruetcse@gmail.com>
2022-09-23 11:44:10 +0600

    Remove hardcoded slider

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -16,20 +16,6 @@ global $post;
 
 		<?php the_content(); ?>
 
-		<div class="slider-wrapper theme-default">
-			<div id="contentSlider" class="content-slider nivoSlider">
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G01.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G02.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G03.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G04.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G05.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G06.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G07.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G08.jpg" alt="Vinaora Nivo Slider 3.x" >
-				<img src="https://www.lonesome-traveler.de/images/animationen/gruppenreisen-en/G19.jpg" alt="Vinaora Nivo Slider 3.x" >
-			</div>
-		</div>
-
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
