9fb1b2e07f06139fde4fd60134a374ba6f8bb47e
Habib Ullah <habib3170@gmail.com>
2022-09-24 12:41:10 +0600

    Main Menu style fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,24 @@ window.TLtrap = (function (window, document, $, undefined) {
 			$('.copy-url-btn').on('click', app.copyToClipboard);
 			app.initMainSlider('#mainSlider');
 			app.initContentSlider('.content-slider');
+			 $('.dropdown-menu > li > .dropdown-menu').parent().addClass('dropdown-submenu').find(' > .dropdown-item').attr('href', '').addClass('dropdown-toggle');
+
+			$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+				e.preventDefault();
+				if (!$(this).next().hasClass('show')) {
+					$(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+				}
+				var $subMenu = $(this).next('.dropdown-menu');
+				$subMenu.toggleClass('show');
+
+
+				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+					e.preventDefault();
+					$('.dropdown-submenu .show').removeClass('show');
+				});
+
+				return false;
+			});
 		},
 		handleSticky: function () {
 			if ($(window).scrollTop() > 400) {
--- header.php
+++ header.php
@@ -55,7 +55,8 @@ defined( 'ABSPATH' ) || exit;
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
-					<?php wp_nav_menu(
+					<?php
+					wp_nav_menu(
 						array(
 								'theme_location'  => 'primary',
 								'container_class' => 'header-navbar-area collapse navbar-collapse navbar-responsive-collapse',
@@ -66,7 +67,9 @@ defined( 'ABSPATH' ) || exit;
 								'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 								'walker'          => new WP_Bootstrap_Navwalker(),
 						)
-					); ?>
+					);
+
+					?>
 				</nav>
 			</div><!-- .container -->
 		</div>
